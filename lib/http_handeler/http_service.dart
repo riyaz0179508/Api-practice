8b2861d38ef14a7ac8236e892915016ceb74f9df
Riyaz Ahmed <riyazahmed2930@gmail.com>
2022-08-29 13:08:02 +0600

    first commit

---
+++ lib/http_handeler/http_service.dart
@@ -0,0 +1,48 @@
+import 'package:dio/dio.dart';
+
+class HttpService {
+  late Dio _dio;
+  final baseUrl = "https://reqres.in/";
+
+  HttpService() {
+    _dio = Dio(BaseOptions(baseUrl: baseUrl));
+    initializeInterceptors();
+  }
+
+
+  Future<Response> getRequest(String endPoint) async {
+    Response response;
+    try {
+      response = await _dio.get(endPoint);
+    } on DioError catch (e) {
+      print(e.message);
+      throw Exception(e.message);
+    }
+
+    return response;
+  }
+
+  Future<Response> postRequest(String endPoint) async {
+    Response response;
+    try {
+      response = await _dio.post(endPoint);
+    } on DioError catch (e) {
+      print(e.message);
+      throw Exception(e.message);
+    }
+
+    return response;
+  }
+
+
+  initializeInterceptors() {
+    _dio.interceptors
+        .add(InterceptorsWrapper(onError: (error, errorInterceptorHandler) {
+      print(error.message);
+    }, onRequest: (request, requestInterceptorHandler) {
+      print('${request.method} ${request.path}');
+    }, onResponse: (response, responseInterceptorsHandeler) {
+      print('${response.statusCode} ${response.statusCode} ${response.data}');
+    }));
+  }
+}
---
+++ lib/model/model.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'model.g.dart';
+
+@JsonSerializable()
+
+class User {
+  @JsonKey(name: "id")
+  late int id;
+
+  @JsonKey(name: "email")
+  late String email;
+
+  @JsonKey(name: "first_name")
+  late String firstName;
+
+  @JsonKey(name: "last_name")
+  late String lastName;
+
+  @JsonKey(name: "avatar")
+  late String avatar;
+  User();
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+}
